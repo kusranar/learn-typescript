8cbca1b2a597f27676bbec850688e4321fbf1d71
sirkusr <sosial.b96@gmail.com>
2020-10-19 10:22:18 +0700

    namespace

---
+++ nine_practice/src/models/drag-drop.ts
@@ -0,0 +1,15 @@
+// drag & drop interfaces
+namespace App  {
+    export interface Draggable {
+      dragStartHandler(event: DragEvent): void;
+      dragEndHandler(event: DragEvent): void;
+    }
+    
+    export interface DragTarget {
+      dragOverHandler(event: DragEvent): void;
+      dragHandler(event: DragEvent): void;
+      dragLeaveHandler(event: DragEvent): void;
+    }
+
+    // class, const can available at namespace
+}
---
+++ nine_practice/src/models/project.ts
@@ -0,0 +1,18 @@
+namespace App {
+    // project type
+  export enum ProjectStatus {
+    Active,
+    Finished,
+  }
+
+  // project type
+  export class Project {
+    constructor(
+      public id: string,
+      public title: string,
+      public description: string,
+      public people: number,
+      public status: ProjectStatus
+    ) {}
+  }
+}
